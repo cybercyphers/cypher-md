aacaf22cf02ee2b0c6dc4551368b5cdaeeed3b93
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-06-14 13:16:41 +0000

    Add ping function to measure latency

--- plugins/ping.js
+++ plugins/ping.js
@@ -1,1 +1,14 @@
 
+const ping = async(sock,jid,msg)=>{
+   const startTime = Date.now();
+    const firstSent = await sock.reply(jid," 🏓 pinging...",msg);
+     const latency = Date.now() - startTime;
+    await sock.reply(jid, `🚅 Latency : ${latency}ms`,firstSent)
+
+}
+
+
+
+
+
+export default ping;
