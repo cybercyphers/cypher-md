8968ad8b0881f5a8afbc4fd3d465dd23abcb50e3
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-06-14 13:21:56 +0000

    Add configFetchJson function to read JSON config

--- libraries/configFetchJson.js
+++ libraries/configFetchJson.js
@@ -1,1 +1,18 @@
 
+import fs from 'node:fs';
+import path,{ dirname } from 'node:path';
+import { fileURLToPath } from 'node:url';
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+
+
+const configFetchJson = (data)=>{
+    const obj = JSON.parse(fs.readFileSync(path.join(__dirname,"../configurations","config.json"),"utf8"));
+    
+     const finalObj = obj[data]
+
+    return finalObj;
+}
+
+
+export default configFetchJson;
