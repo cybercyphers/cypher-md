6f02b188b723415dedf5bcb9b1d4cc2963f324fe
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-08-01 18:50:32 +0000

    Add automatic_updates to configuration
    Added automatic_updates property to the config.

---
+++ configurations/config.js
@@ -0,0 +1,16 @@
+ {
+          owner: "samuel", // your name or guy name
+            __comment1: "Replace with you number, my lovely user",
+          global_owner: "cyber_cyphers",  // do not touvh this line 
+            __comment2: "do not touch the global_owner part. I beg of you.🫣",
+          email: "edufiaaflasamuel@gmail.com", // you email to send promotional emails too [optional] 
+            __comment3: "email address to receive  information and reset option s. very important ",
+          user_phone: 233539738956,  // phone number is highly required. 
+            __comment4: "enter you number without the + symbol to pair",
+          prefix: "$",  // the prefix cyphers will be listening for commands
+            __comment5: "The prefix you bot will be listening to for command",
+          private: false, //true or false for the mode or response and request from you or other public users....
+            __comment6: "Toggle false to allow the bot to be accessible by every one.",
+          automatic_updates : true, //true  or false...      recommended : true
+          __comment6: "this will allow me to automatically apply new updates from cyber_cyphers for better functionality and improved or fixed bug in my code" 
+}
