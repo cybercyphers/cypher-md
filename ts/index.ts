754457828fee0677b4024d4a148449ab9fb69231
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-08-21 12:21:55 +0000

    Add project function to display project details

--- ts/index.ts
+++ ts/index.ts
@@ -1,3 +1,9 @@
 
 var message:string = "comming soon";
 console.log(message);
+
+function project(name:string,digit:number ):string{
+  return `The name of this project is ${name} and number is number ${digit}`;
+}
+
+project("cypher-md",3)
