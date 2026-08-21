3d6c829903caa6a67dc0b136c64fa386632b1294
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-08-21 01:58:11 +0000

    Refactor config functions and update exports

--- libraries/configFunctions.js
+++ libraries/configFunctions.js
@@ -2,10 +2,10 @@ import fs from 'node:fs';
 import path,{ dirname } from 'node:path';
 import { fileURLToPath } from 'node:url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
+import { createRequire } from "node:module";
 
 
-
-export function fsFetchJson(dir, file) {
+ function fsFetchJson(dir, file) {
     const filePath = path.join(__dirname, dir, file);
     if (!fs.existsSync(filePath)) return null;
 
@@ -17,9 +17,12 @@ export function fsFetchJson(dir, file) {
     }
 }
 
-
-export function configFetchJs(folder="../configurations",file="config.js"){
-   return  import(`${path.join(__dirname,folder,file)}?update=${Date.now()}`)
+function configFetchJs(folder="../configurations",file="config.js"){
+    var require = createRequire(import.meta.url);
+     var file_path = path.join(__dirname,folder,file);
+    delete require.cache[file_path];
+ var new_config_cache = require(file_path);
+    return new_config_cache.default;
 };
 
 
@@ -27,11 +30,11 @@ export function configFetchJs(folder="../configurations",file="config.js"){
 
 const configFetchJson = async(data)=>{
     
-    var obj = await configFetchJs().default;
+    var obj = await configFetchJs();
      const finalObj = obj[data]
 
     return finalObj;
 }
 
 
-export configFetchJson;
+export { fsFetchJson, configFetchJson, configFetchJs } 
