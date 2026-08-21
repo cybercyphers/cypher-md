6d6f0b1895c851562f543971e0667c9523f0dab0
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-08-21 17:04:50 +0000

    Add primary key and not null constraint to cypher_user

--- SQL/ciph_schema.sql
+++ SQL/ciph_schema.sql
@@ -1,6 +1,7 @@
 
 CREATE TABLE IF NOT EXISTS cypher_user(
-    name TEXT, 
+    id INTEGER PRIMARY KEY NOT NULL,
+    name TEXT NOT NULL, 
     version TEXT,
     engine TEXT,
     online BOOLEAN DEFAULT false, 
