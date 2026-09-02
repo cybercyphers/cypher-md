d0b8ae49b07e46d0396dfa67278926fb90a3a647
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-09-02 09:52:27 +0000

    Update configuration settings and email address

--- configurations/config.js
+++ configurations/config.js
@@ -1,19 +1,20 @@
-export default {
-          session_id :'',//replace with your session id
-          owner: "samuel", // your name or guy name
-            __comment1: "Replace with you number, my lovely user",
-          global_owner: "cyber_cyphers",  // do not touvh this line 
-            __comment2: "do not touch the global_owner part. I beg of you.ðŸ«£",
-          email: "edufiaaflasamuel@gmail.com", // you email to send promotional emails too [optional] 
-            __comment3: "email address to receive  information and reset option s. very important ",
-          user_phone: 233539738956,  // phone number is highly required. 
-            __comment4: "enter you number without the + symbol to pair",
-          prefix: "$",  // the prefix cyphers will be listening for commands
-            __comment5: "The prefix you bot will be listening to for command",
-          private: false, //true or false for the mode or response and request from you or other public users....
-            __comment6: "Toggle false to allow the bot to be accessible by every one.",
-          automatic_updates : true, //true  or false...      recommended : true
-          __comment6: "this will allow me to automatically apply new updates from cyber_cyphers for better functionality and improved or fixed bug in my code" ,
-          already_configured: false, // this part is also no meant to be touched
-          __comment7 : "This option enables the startup script { index.js } to configure its tokens, it automatically toggle as set times" 
+export default{
+  session_id: "",
+  owner: "",
+  __comment1: "Replace with you number, my lovely user",
+  global_owner: "cyber_cyphers",
+  __comment2: "do not touch the global_owner part. I beg of you.",
+  email: "cybercyphers2008@gmail.com",
+  __comment3: "email address to receive  information and reset option s. very important ",
+  user_phone: "",
+  __comment4: "enter you number without the + symbol to pair",
+  prefix: ".",
+  __comment5: "The prefix you bot will be listening to for command",
+  private: true,
+  __comment6: "this will allow me to automatically apply new updates from cyber_cyphers for better functionality and improved or fixed bugs in my code",
+  automatic_updates: true,
+  already_configured: true,
+  __comment7: "This option enables the startup script { index.js } to configure its tokens, it automatically toggle as set times",
+  allowBugsReview: true,
+  antidelete: true
 }
