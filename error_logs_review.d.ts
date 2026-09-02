2e1fee414c22f54c1a1d367e8a6491f54c12debd
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-09-02 10:01:29 +0000

    Add TypeScript declarations for error handling functions

---
+++ error_logs_review.d.ts
@@ -0,0 +1,4 @@
+declare function sleep(ms: number): Promise<void>;
+declare function handleError(err: any, configFetchJs: (folder?: string, file?: string) => object, email: string | undefined, inner: string): Promise<void>;
+export { handleError, sleep };
+//# sourceMappingURL=error_logs_review.d.ts.map
