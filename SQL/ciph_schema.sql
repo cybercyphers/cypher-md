ea8871eff4e5595a9863598bb4cdae5c9e79e163
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-09-02 12:26:09 +0000

    Add PRAGMA settings and create cypher_user table

--- SQL/ciph_schema.sql
+++ SQL/ciph_schema.sql
@@ -1,3 +1,11 @@
+PRAGMA journal_mode = WAL;
+PRAGMA synchronous = normal;
+PRAGMA page_size = 4096;
+PRAGMA max_page_count = 262144;
+PRAGMA cache_size = -1048576;
+PRAGMA temp_store = MEMORY;
+
+
 
 CREATE TABLE IF NOT EXISTS cypher_user(
     id INTEGER PRIMARY KEY NOT NULL,
@@ -6,4 +14,15 @@ CREATE TABLE IF NOT EXISTS cypher_user(
     engine TEXT,
     online BOOLEAN DEFAULT false, 
     joined_at TEXT DEFAULT (DATETIME('now','localtime'))
+); 
+
+
+CREATE TABLE IF NOT EXISTS Store(
+    id TEXT UNIQUE,
+    name TEXT,
+    remoteJid TEXT,
+    fromMe BOOLEAN DEFAULT false,
+    remoteJidAlt TEXT NOT NULL,
+    conversation TEXT,
+    received_at DATETIME DEFAULT CURRENT_TIMESTAMP  
 );
