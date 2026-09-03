085a19277ffa3bec745e5a966c4d79924149f730
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-09-03 22:47:32 +0000

    Document parameters for obfuscation logic
    Added parameter documentation for obfuscation function.

--- Workers/ob.js
+++ Workers/ob.js
@@ -2,10 +2,16 @@ import { seek } from "../libraries/brainiac.m.js"
 import { parentPort, workerData } from "worker_threads";
 
 //console.log(seek)
+/*
+@param { buffer } (bufferData) - the javascript file converted to buffer;
+@param { string } (level) - the level of obfuscation
+*/
 var { bufferData,level } = workerData;
 //console.log(bufferData,level)
  //main obfuscation logic begins 
-    try{
+    
+
+try{
     //obfuscation logic ends;
         //console.log("obfuscating...")
     var obfuscated = await seek(bufferData,level);
