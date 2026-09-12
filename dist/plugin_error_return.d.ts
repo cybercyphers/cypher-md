f3d20bb92124f25da88459c0fc85ad90d5a7de8a
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-09-12 18:42:05 +0000

    Add type definition for Err function

---
+++ dist/plugin_error_return.d.ts
@@ -0,0 +1,3 @@
+import { proto } from "@whiskeysockets/baileys";
+export default function Err(sock: any, jid: string, msg: object | string, command: string): Promise<proto.IWebMessageInfo | undefined | string>;
+//# sourceMappingURL=plugin_error_return.d.ts.map
