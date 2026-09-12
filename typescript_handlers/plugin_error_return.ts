36fdd2dc69e22306beee2b791552677cd2b646a8
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-09-12 18:26:34 +0000

    Add error handling function for message processing

---
+++ typescript_handlers/plugin_error_return.ts
@@ -0,0 +1,20 @@
+import { proto } from "@whiskeysockets/baileys";
+
+
+import { configFetchJs } from "../libraries/configFunctions.js";
+
+export default async function Err(sock: any, jid:string,msg: object|string,command: string): Promise<proto.IWebMessageInfo | undefined | string>{
+    try{
+        
+       var c_convert = command.replace(configFetchJs().prefix,""); 
+        
+  var errorInfoReturn: string = `🙄„Oops, A Bug was caught in my code while processing ${c_convert}, I will send the error to administration for review anytime now.*`;
+        return await sock.reply(jid,errorInfoReturn,msg);
+
+}catch{
+    console.log("Error system failed to send....");
+    return "failed"
+}
+
+
+}
