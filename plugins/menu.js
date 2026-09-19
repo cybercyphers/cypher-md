81b1468dd52ef1a05bd214973f7d4bbc3839bda3
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-09-19 23:15:21 +0000

    Enhance menu formatting and system info display

--- plugins/menu.js
+++ plugins/menu.js
@@ -1,3 +1,6 @@
+"use strict"
+
+
 import os from "node:os";
 import { configFetchJs } from "../libraries/configFunctions.js";
 import path,{ dirname } from "node:path";
@@ -5,40 +8,88 @@ import { fileURLToPath } from "node:url";
 
 var __dirname = dirname(fileURLToPath(import.meta.url));
 
+
+/*
+var MINUTES = Math.floor((uptime/3600)%60);
+var SECONDS = Math.floor((uptime)%60);
+*/
+
 const menu = async(sock,jid,msg)=>{
-    const menu = `╭━━━〔 CYPHER-MD 〕━━━⬣\n` +
-`┃ 👤 Owner : ${configFetchJs().owner} \n` +
-`┃ ⚡ Prefix : ${configFetchJs().prefix}\n` +
-`┃ 🖥️ Platform : ${process.platform}\n` +
-`┃ 🟢 Status : Online\n` +
+    try{
+        
+    const menu = `╭━━━〔 *CYPHER-MD* 〕━━━⬣\n` +
+`┃ 👤 Owner : *${configFetchJs().owner}*\n` +
+`┃ ➢ Prefix: *${configFetchJs().prefix}*\n`+
+`┃ ➢ Mode : *${configFetchJs().private ? "Private" : "Public"}*\n` +  
+`┃ ➢ Machine : *${os.machine()}*\n` + 
+`┃ ➢ Total Ram: *${(os.totalmem()/(1024*1024*1024)).toFixed(2)}GB*\n` +
+`┃ ➢ Free Memory : *${(os.freemem()/(1024*1024*1024)).toFixed(2)}GB*\n` + 
+`┃ ➢ Used Memory : *${((os.totalmem()-os.freemem())/(1024*1024*1024)).toFixed(2)}GB*\n` +   
+`┃ ➢ Platform : *${process.platform}*\n` +
+`┃ ➢ OS version : *${os.release()}*\n` +     
+`┃ 🟢 Status : *Online*\n` +
 `╰━━━━━━━━━━━━━━━━⬣\n\n` +
 
-`╭━━━〔 📋 GENERAL 〕━━━⬣\n` +
-`┃  .ping\n` +
-`┃  .menu\n` +
-`┃  .alive\n` +
-`┃  .info\n` +
-`┃  .help\n` +
+`╭━━━〔 📋 *\`\`\`GENERAL\`\`\`* 〕━━━⬣\n` +
+`┃  *⊛ ping*\n` +
+`┃  *⊛ menu*\n` +
+`┃  *⊛ info*\n` +
+`┃  *⊛ help*\n` +
+`┃  *⊛ version*\n` +
+`┃  *⊛ update*\n` +
+`┃  *⊛ developer*\n` +       
+`╰━━━━━━━━━━━━━━━━⬣\n\n` +  
+          
+ `╭━━〔 📋 *\`\`\`INTELLIGENCE\`\`\`* 〕━⬣\n` +
+`┃  *⊛ CypherAi [on/off]*\n` +     
+`┃  *⊛ ClaudeOpus-4.5*\n` +    
+`┃  *⊛ gpt5*\n` +
+`┃  *⊛ gpt5.5*\n` +
+`┃  *⊛ gpt5-nano*\n` +
+`┃  *⊛ grok4.1*\n` +
+`┃  *⊛ deepseek*\n` +
+`┃  *⊛ qwen3*\n` +       
+`╰━━━━━━━━━━━━━━━━⬣\n\n` +                   
+`╭━━━〔 *\`\`\`ENCRYPTIONS\`\`\`* 〕━━━⬣\n` +
+`┃  *⊛ setkeys*\n` +      
+`┃  *⊛ getkeys*\n` +
+`┃  *⊛ encryptHex*\n`+
+`┃  *⊛ encryptBase64*\n` +
+`┃  *⊛ decrypt*\n` +
+`┃  *⊛ SHA256*\n` + 
+`┃  *⊛ SHA512*\n` +
+`╰━━━━━━━━━━━━━━━━⬣\n\n` +
+          
+`╭━━━〔 *\`\`\`NEWS\`\`\`* 〕━━━⬣\n` +
+`┃  *⊛ aljazeera*\n` +     
+`╰━━━━━━━━━━━━━━━━⬣\n\n` + 
+          
+`╭━━━〔 *\`\`\`EXTRACTS\`\`\`* 〕━━━⬣\n` +
+`┃  *⊛ vv*\n` +     
+`╰━━━━━━━━━━━━━━━━⬣\n\n` +  
+          
+`╭━━〔 *\`\`\`AUTOMATIONS\`\`\`* 〕━⬣\n` +
+`┃  *⊛ antidelete on/off*\n` +      
+`┃  *⊛ autotyping on/off*\n` +
+`┃  *⊛ autorecording on/off*\n` +
+`┃  *⊛ autostatusreact on/off*\n` + `┃  *⊛ autoviewstatus on/off*\n` +   
+`╰━━━━━━━━━━━━━━━━⬣\n\n` +               
+`╭━━━〔 *\`\`\`SEEKERS\`\`\`* 〕━━━⬣\n` +
+`┃  *⊛ obfuscate -low*\n` +
+`┃  *⊛ obfuscate -medium*\n` +
+`┃  *⊛ obfuscate -high*\n` +
+`╰━━━━━━━━━━━━━━━━⬣\n\n` +
+          
+`╭━━━〔 👑 *\`\`\`OWNER\`\`\`* 〕━━━⬣\n` +
+`┃  *⊛ mode private*\n` +
+`┃  *⊛ mode public*\n` +
+`┃  *⊛ restart -pending*\n` +
 `╰━━━━━━━━━━━━━━━━⬣\n\n` +
 
-`╭━━━〔 👥 GROUP 〕━━━⬣\n` +
-`┃  .add\n` +
-`┃  .kick\n` +
-`┃  .promote\n` +
-`┃  .demote\n` +
-`┃  .tagall\n` +
-`╰━━━━━━━━━━━━━━━━⬣\n\n` +
-
-`╭━━━〔 👑 OWNER 〕━━━⬣\n` +
-`┃  .mode private\n` +
-`┃  .mode public\n` +
-`┃  .restart\n` +
-`╰━━━━━━━━━━━━━━━━⬣\n\n` +
-
-`╭━━━〔 ⚙️ SYSTEM 〕━━━⬣\n` +
-`┃ 🚀 socket : Baileys\n` +
-`┃ 💻 Runtime : ${process.uptime()}\n` +
-`┃ 🔥 Fast & Stable\n` +
+`╭━━━〔 ⚙️ *\`\`\`SYSTEM\`\`\`* 〕━━━⬣\n` +
+`┃ 🚀 socket : *Baileys*\n` +
+`┃ 💻 Runtime : *${Math.floor(process.uptime()/3600)}h(s) ${Math.floor((process.uptime()%3600)/60)}m ${Math.floor(process.uptime()%60)}s*\n` +
+`┃ 🔥 *Fast & Stable*\n` +
 `╰━━━━━━━━━━━━━━━━⬣\n\n` 
 ;
 
@@ -48,16 +99,13 @@ const menu = async(sock,jid,msg)=>{
     
  await sock.sendMessage(jid,{
 text:"Loading..."
-},{ quoted:msg });
-    
-    
-   await new Promise(resolve => setTimeout(resolve,100));
-    
+ },{ quoted:msg});
+      
     await sock.sendMessage(jid,{
 image:{ url:path.join(__dirname,"../assets","cyph_256.jpeg")},
 caption: menu
     },{ quoted:msg});
     
-
+   }catch(e){ console.log("[\x1b[31mFailed tooad menu\x1b[0m]",e) }
 }
 export default menu;
