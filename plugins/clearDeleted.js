92fbe17395e7a135e9dccceb1904d9664cc1e670
cyber <193218221+cybercyphers@users.noreply.github.com>
2026-09-19 22:52:45 +0000

    Update clearDeleted.js

--- plugins/clearDeleted.js
+++ plugins/clearDeleted.js
@@ -1,22 +1,27 @@
-async function clearOld(db){
+async function clearOld(db,image_db,cypherAiDb){
+    
+    var databases = [db,image_db,cypherAiDb];
+    
     setInterval(async()=>{
-        console.log("[\x1b[1;34mChecking for trashes in database...\x1b[0m]");
-        
-     var Deleted = await db.prepare("DELETE FROM Store WHERE received_at < Datetime('now','-1 day')").run();
-        
-        if(Deleted.changes > 0){
-      console.log("[\x1b[1;36mDatabase trash Clean_up was successful...\x1b[0m]");
-};
-        
-  },1000 * 60 * 60 * 25);
+        console.log("[\x1b[1;36m🗑️Searching for trashes in database...\x1b[0m]");
+  
+   for(const store of databases){
+       store.pragma("wal_checkpoint(TRUNCATE)");
+       store.prepare("VACUUM").run();
+       
+    store.prepare("DELETE FROM Store WHERE received_at < Datetime('now','-1 day')").run();
+       
+       if(store.changes > 0 ){
+      console.log("[\x1b[1;36m🚮Database trash Clean_up was successful...\x1b[0m]");
+}else{
+    console.log("[\x1b[1;36m The Databases are clean, aborting...\x1b[0m]");  
+} 
+
+ }
+              
+  },1000 * 60 * 60 * 24);
   
 };
 
 
 
-
-
-
-
-
-export default clearOld;
